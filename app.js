4020c4b7cd578b16efb212287d334dcbfa342868
Dmitry Bulgakov <mail@dbulgakov.com>
2016-05-07 15:12:32 +0300

    Updated port

--- app.js
+++ app.js
@@ -17,6 +17,6 @@ app.use(function (req, res) {
 })
 
 
-app.listen(3000, function(){
-    console.log('Express server listening on port 3000');
+app.listen(80, function(){
+    console.log('Express server listening on port 80');
 });
