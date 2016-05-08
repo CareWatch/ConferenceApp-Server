2e774c5fefe06569529a60be271bef7f77ab8f56
Dmitry Bulgakov <mail@dbulgakov.com>
2016-05-08 18:20:53 +0300

    Updated config files, server configuration

--- app.js
+++ app.js
@@ -1,31 +1,31 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+var express = require('express'),
+    path = require('path'),
+    favicon = require('serve-favicon'),
+    logger = require('morgan'),
+    bodyParser = require('body-parser'),
+    config = require('./configuration'),
+    morgan = require('morgan'),
+    routes = require('./routes/index'),
+    users = require('./routes/users'),
+    fs = require('fs');
+    app = express();
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 
-var app = express();
+var accessLogStream = fs.createWriteStream(config.logPath, {flags: 'a'});
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-// uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+app.use(morgan('combined', {stream: accessLogStream}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
+
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Myconf.guru main page. Waiting for an update.' });
 });
 
 module.exports = router;
