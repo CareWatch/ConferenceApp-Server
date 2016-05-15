f04c1d9d2d532a7ecf6cf0f2190a35644a18f3c1
Dmitry Bulgakov <mail@dbulgakov.com>
2016-05-15 11:25:44 +0300

    Added appRoot package to dial with routes in app

--- configuration/index.js
+++ configuration/index.js
@@ -1,10 +1,11 @@
 var fs = require('fs'),
-    config = {};
+    config = {},
+    appRoot = require('app-root-path');
 
 config.port = 3000;
 config.sslPort = 3001;
 
-config.logPath = '../logs/access.log';
+config.logPath = appRoot + '/logs/access.log';
 
 config.sslCa = fs.readFileSync('/etc/ssl/keys/server.ca-bundle');
 config.sslKey = fs.readFileSync('/etc/ssl/keys/server.key');
