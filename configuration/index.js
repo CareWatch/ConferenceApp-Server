ef86cf42d45596d078066f0fbcfac72b7bba2503
Dmitry Bulgakov <mail@dbulgakov.com>
2016-06-28 12:15:23 +0300

    Minor update due to move to nginx proxy

--- configuration/index.js
+++ configuration/index.js
@@ -3,19 +3,12 @@ var appRoot = require('app-root-path');
 var config = {};
 
 
-config.port = 3000;
-config.sslPort = 3001;
+config.port = 3001;
 
 config.appRoot = appRoot;
 
 config.accessLogsPath = appRoot + '/logs/access.log';
 config.loggerLogsPath = appRoot + '/logs/app.log';
-
-
-config.sslCa = fs.readFileSync('/etc/ssl/keys/server.ca-bundle');
-config.sslKey = fs.readFileSync('/etc/ssl/keys/server.key');
-config.sslCert = fs.readFileSync('/etc/ssl/keys/server.crt');
-config.sslPassphrase = '';
 
 config.dbString = '';
 config.jwtSecret = '';
